3fcfa488b8f2681ec101d3a0a17617b28f986a97
huangdouya-tim <763030040@qq.com>
2026-04-03 18:53:53 +0800

    add /tick command

--- src/main/java/dev/dubhe/curtain/commands/TickCommand.java
+++ src/main/java/dev/dubhe/curtain/commands/TickCommand.java
@@ -1,85 +1,172 @@
 package dev.dubhe.curtain.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.arguments.DoubleArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import dev.dubhe.curtain.features.rules.fakes.TickManagerProvider;
-import dev.dubhe.curtain.utils.TickManager;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import dev.dubhe.curtain.CurtainRules;
+import dev.dubhe.curtain.features.rules.fakes.MinecraftServerInterface;
+import dev.dubhe.curtain.network.ServerNetworkHandler;
+import dev.dubhe.curtain.utils.CommandHelper;
+import dev.dubhe.curtain.utils.CurtainProfiler;
+import dev.dubhe.curtain.utils.Messenger;
+import dev.dubhe.curtain.utils.ServerTickRateManager;
 import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.TextComponent;
-import net.minecraft.server.MinecraftServer;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.Nullable;
+
+import static com.mojang.brigadier.arguments.FloatArgumentType.floatArg;
+import static com.mojang.brigadier.arguments.FloatArgumentType.getFloat;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.getInteger;
+import static com.mojang.brigadier.arguments.IntegerArgumentType.integer;
+import static com.mojang.brigadier.arguments.StringArgumentType.getString;
+import static com.mojang.brigadier.arguments.StringArgumentType.greedyString;
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
+import static net.minecraft.commands.SharedSuggestionProvider.suggest;
 
 public class TickCommand {
-    public static void register(@NotNull CommandDispatcher<CommandSourceStack> dispatcher) {
-        dispatcher.register(Commands.literal("tick")
-                .requires(source -> source.hasPermission(2))
-                .then(Commands.literal("rate")
-                        .then(Commands.argument("tps", DoubleArgumentType.doubleArg(1, 500))
-                                .executes(ctx -> setRate(ctx.getSource(), DoubleArgumentType.getDouble(ctx, "tps")))))
-                .then(Commands.literal("warp")
-                        .then(Commands.argument("ticks", IntegerArgumentType.integer(1))
-                                .executes(ctx -> warp(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "ticks")))))
-                .then(Commands.literal("freeze")
-                        .executes(ctx -> freeze(ctx.getSource())))
-                .then(Commands.literal("step")
-                        .executes(ctx -> step(ctx.getSource(), 1))
-                        .then(Commands.argument("ticks", IntegerArgumentType.integer(1))
-                                .executes(ctx -> step(ctx.getSource(), IntegerArgumentType.getInteger(ctx, "ticks")))))
-                .then(Commands.literal("query")
-                        .executes(ctx -> query(ctx.getSource()))));
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher)
+    {
+        LiteralArgumentBuilder<CommandSourceStack> literalargumentbuilder = literal("tick").
+                requires((player) -> CommandHelper.canUseCommand(player, CurtainRules.commandLog)).
+                then(literal("rate").
+                        executes((c) -> queryTps(c.getSource())).
+                        then(argument("rate", floatArg(0.1F, 500.0F)).
+                                suggests( (c, b) -> suggest(new String[]{"20.0"},b)).
+                                executes((c) -> setTps(c.getSource(), getFloat(c, "rate"))))).
+                then(literal("warp").
+                        executes( (c)-> setWarp(c.getSource(), 0, null)).
+                        then(argument("ticks", integer(0)).
+                                suggests( (c, b) -> suggest(new String[]{"3600","72000"},b)).
+                                executes((c) -> setWarp(c.getSource(), getInteger(c,"ticks"), null)).
+                                then(argument("tail command", greedyString()).
+                                        executes( (c) -> setWarp(
+                                                c.getSource(),
+                                                getInteger(c,"ticks"),
+                                                getString(c, "tail command")))))).
+                then(literal("freeze").executes( (c)-> toggleFreeze(c.getSource(), false)).
+                        then(literal("status").executes( (c) -> freezeStatus(c.getSource()))).
+                        then(literal("deep").executes( (c) -> toggleFreeze(c.getSource(), true))).
+                        then(literal("on").executes( (c) -> setFreeze(c.getSource(), false, true)).
+                                then(literal("deep").executes( (c)-> setFreeze(c.getSource(), true, true)))).
+                        then(literal("off").executes( (c) -> setFreeze(c.getSource(), false, false)))).
+                then(literal("step").
+                        executes((c) -> step(c.getSource(), 1)).
+                        then(argument("ticks", integer(1,72000)).
+                                suggests( (c, b) -> suggest(new String[]{"20"},b)).
+                                executes((c) -> step(c.getSource(), getInteger(c,"ticks"))))).
+                then(literal("superHot").executes( (c)-> toggleSuperHot(c.getSource()))).
+                then(literal("health").
+                        executes( (c) -> healthReport(c.getSource(), 100)).
+                        then(argument("ticks", integer(20,24000)).
+                                executes( (c) -> healthReport(c.getSource(), getInteger(c, "ticks"))))).
+                then(literal("entities").
+                        executes((c) -> healthEntities(c.getSource(), 100)).
+                        then(argument("ticks", integer(20,24000)).
+                                executes((c) -> healthEntities(c.getSource(), getInteger(c, "ticks")))));
+
+
+        dispatcher.register(literalargumentbuilder);
     }
 
-    private static int setRate(CommandSourceStack source, double tps) {
-        TickManager manager = getTickManager(source.getServer());
-        manager.setTargetTps(tps);
-        source.sendSuccess(new TextComponent("tick rate set to" + tps + "TPS"), true);
+
+    private static int setTps(CommandSourceStack source, float tps)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        trm.setTickRate(tps, true);
+        queryTps(source);
+        return (int)tps;
+    }
+
+    private static int queryTps(CommandSourceStack source)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+
+        Messenger.m(source, "w Current tps is: ",String.format("wb %.1f", trm.tickrate()));
+        return (int) trm.tickrate();
+    }
+
+    private static int setWarp(CommandSourceStack source, int advance, String tail_command)
+    {
+        @Nullable Entity entity = source.getEntity();
+        @Nullable ServerPlayer player = (entity instanceof ServerPlayer) ? (ServerPlayer) entity : null; // may be null
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        Component message = trm.requestGameToWarpSpeed(player, advance, tail_command, source);
+        source.sendSuccess(message, false);
         return 1;
     }
 
-    private static int step(CommandSourceStack source, int ticks) {
-        TickManager manager = getTickManager(source.getServer());
-        if (!manager.isFrozen()) {
-            manager.setFrozen(true);
+    private static int freezeStatus(CommandSourceStack source)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        if(trm.gameIsPaused())
+        {
+            Messenger.m(source, "gi Freeze Status: Game is "+(trm.deeplyFrozen()?"deeply ":"")+"frozen");
         }
-        manager.setStepRemaining(ticks);
-        source.sendSuccess(new TextComponent("Stepping" + ticks + "tick(s)"), true);
-        return 1;
-    }
-
-    private static int warp(CommandSourceStack source, int ticks) {
-        TickManager manager = getTickManager(source.getServer());
-        manager.setWarpRemaining(ticks);
-        source.sendSuccess(new TextComponent("Warping" + ticks + "ticks"), true);
-        return 1;
-    }
-
-    private static int freeze(CommandSourceStack source) {
-        TickManager manager = getTickManager(source.getServer());
-        if (manager.isFrozen()) {
-            source.sendSuccess(new TextComponent("Tick freezing"), true);
-            manager.setFrozen(false);
-        } else {
-            source.sendSuccess(new TextComponent("Tick runs normally"), true);
-            manager.setFrozen(true);
+        else
+        {
+            Messenger.m(source, "gi Freeze Status: Game runs normally");
         }
         return 1;
     }
-    private static int query(CommandSourceStack source) {
-        MinecraftServer server = source.getServer();
-        TickManager manager = getTickManager(server);
-        double targetTps = manager.getTargetTps();
-        Boolean frozen = manager.isFrozen();
-        int warp = manager.getWarpRemaining();
-        int step = manager.getStepRemaining();
-        double actualTps = 1000.0/Math.max(server.getAverageTickTime(), 1);
-        String msg = String.format("Target TPS: %.1f, Actual TPS: %.2f, Frozen: %s, warp: %d, Step: %d",
-                targetTps, actualTps, frozen, warp, step);
-        source.sendSuccess(new TextComponent(msg), false);
+
+    private static int setFreeze(CommandSourceStack source, boolean isDeep, boolean freeze)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        trm.setFrozenState(freeze, isDeep);
+        if (trm.gameIsPaused())
+        {
+            Messenger.m(source, "gi Game is "+(isDeep?"deeply ":"")+"frozen");
+        }
+        else
+        {
+            Messenger.m(source, "gi Game runs normally");
+        }
         return 1;
     }
-    private static TickManager getTickManager(MinecraftServer server) {
-        return ((TickManagerProvider) server).getTickManager();
+
+    private static int toggleFreeze(CommandSourceStack source, boolean isDeep)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        return setFreeze(source, isDeep, !trm.gameIsPaused());
     }
+
+    private static int step(CommandSourceStack source, int advance)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        trm.stepGameIfPaused(advance);
+        Messenger.m(source, "gi Stepping " + advance + " tick" + (advance != 1 ? "s" : ""));
+        return 1;
+    }
+
+    private static int toggleSuperHot(CommandSourceStack source)
+    {
+        ServerTickRateManager trm = ((MinecraftServerInterface)source.getServer()).getTickRateManager();
+        trm.setSuperHot(!trm.isSuperHot());
+        ServerNetworkHandler.updateSuperHotStateToConnectedPlayers(source.getServer());
+        if (trm.isSuperHot())
+        {
+            Messenger.m(source, "gi Superhot enabled");
+        }
+        else
+        {
+            Messenger.m(source, "gi Superhot disabled");
+        }
+        return 1;
+    }
+
+    public static int healthReport(CommandSourceStack source, int ticks)
+    {
+        CurtainProfiler.prepare_tick_report(source, ticks);
+        return 1;
+    }
+
+    public static int healthEntities(CommandSourceStack source, int ticks)
+    {
+        CurtainProfiler.prepare_entity_report(source, ticks);
+        return 1;
+    }
+
+
 }
